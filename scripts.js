49ddfa950ada0861f29b4dcc7222b0265c8f9c86
Justin <justin.zachow@gmail.com>
2020-09-18 22:01:22 -0400

    First final draft done

---
+++ scripts.js
@@ -0,0 +1,11 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+$(document).ready(function () {
+    $('#down').on('click', function () {
+        $("html").scrollTop(0); $('html, body').animate({
+            scrollTop: $('#about').offset().top
+        }, 1000);
+    });
+});
